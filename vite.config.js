82654b2b9d836ff8822f733bbde99c21e7c60bc4
Michael Tsao <superman8914@gmail.com>
2025-09-24 16:16:42 +0800

    Configure GitHub Actions for automated deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ai-wardrobe-vite/',
+  base: '/Virtual-Wardrobe/',
   build: {
     outDir: 'dist',
   },
